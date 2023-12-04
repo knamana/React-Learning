52a3d61dca4806ce95f46d04eb5a92788e7b4919
knamana <knamana.code@gmail.com>
2023-12-03 19:34:13 -0500

    adds sibling tag into the child div using array

--- App.js
+++ App.js
@@ -2,19 +2,25 @@ const parent = React.createElement("div",
     {id: "parent"}, 
     React.createElement("div", 
         {id: "child"},
-        React.createElement("h1", 
+        [
+            React.createElement("h1", 
             {}, 
             "Hello from H1 tag!"
-        )
+            ), React.createElement("h2", 
+            {}, 
+            "Hello from H2 tag!"
+            )
+        ]
     )    
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
 
-// creating a nested structure 
+// creating a nested structure and adding siblings in child div
 // <div id="parent">
 //  <div id="child">
 //      <h1></h1>
+//      <h2></h2>
 //  </div>
 // </div>
