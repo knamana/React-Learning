dca8c58a92276877c7f9894189ad2ecaa7b0dcfa
knamana <knamana.code@gmail.com>
2023-12-03 19:26:41 -0500

    Creating a nested struture of html tags

--- App.js
+++ App.js
@@ -1,4 +1,20 @@
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+const parent = React.createElement("div", 
+    {id: "parent"}, 
+    React.createElement("div", 
+        {id: "child"},
+        React.createElement("h1", 
+            {}, 
+            "Hello from H1 tag!"
+        )
+    )    
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
+
+// creating a nested structure 
+// <div id="parent">
+//  <div id="child">
+//      <h1></h1>
+//  </div>
+// </div>
