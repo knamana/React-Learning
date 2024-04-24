7d55fa6626ca2397740825dd3370dff534da001f
knamana <knamana.code@gmail.com>
2024-04-23 21:06:16 -0400

    using jsx

--- App.js
+++ App.js
@@ -1,29 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", 
-    {id: "parent"}, 
-    React.createElement("div", 
-        {id: "child"},
-        [
-            React.createElement("h1", 
-            {}, 
-            "Hello from H1 tag!"
-            ), React.createElement("h2", 
-            {}, 
-            "Hello from H2 tag!"
-            )
-        ]
-    )    
-);
+// using JSX
+const jsxHeading = <h1>Namasthe React with JSX!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-
-// creating a nested structure and adding siblings in child div
-// <div id="parent">
-//  <div id="child">
-//      <h1></h1>
-//      <h2></h2>
-//  </div>
-// </div>
+root.render(jsxHeading);
