5fa6d3dde72efa5cc71ba23e21c8f9d4fa3ffdb9
knamana <knamana.code@gmail.com>
2024-04-27 21:34:45 -0400

    completes Assignment-3 part-1

--- App.js
+++ App.js
@@ -1,19 +1,71 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React component
-const Title = () => (
-    <h1>Namasthe React!</h1>
-);
+// Using React.createElement
+// const header = React.createElement("div", 
+//     {class: "title"}, 
+//     [
+//         React.createElement("h1", 
+//         {}, 
+//         "Hello from H1 tag!"
+//         ), React.createElement("h2", 
+//         {}, 
+//         "Hello from H2 tag!"
+//         ), React.createElement("h3", 
+//         {}, 
+//         "Hello from H3 tag!"
+//         )
+//     ]    
+// );
 
-const HeadingComponent = () => (
-    <div>
-        <Title />
-        <h1>Namasthe React with Functional component!</h1>
-    </div>
-);
+// Using JSX
+// const header = (
+//     <div className="title">
+//         <h1>Hello from H1 tag!</h1>
+//         <h2>Hello from H2 tag!</h2>
+//         <h3>Hello from H3 tag!</h3>
+//     </div>
+// );
 
+// using Functional component
+// const HeaderComponent = () => {
+//     return (
+//         <div className="title">
+//             <h1>Hello from H1 tag!</h1>
+//             <h2>Hello from H2 tag!</h2>
+//             <h3>Hello from H3 tag!</h3>
+//         </div>
+//     );
+// };
+
+// Compositional component
+// const AnotherComponent = () => (
+//     <div>
+//         <HeaderComponent/>
+//         <h1>This is another component!</h1>
+//     </div>
+// );
+
+// `{TitleComponent}` vs `{<TitleComponent/>}` vs `{<TitleComponent></TitleComponent>}` in JSX.
+const element = <h1>This is an react element</h1>
+
+const TitleFnComponent = () => {
+    return <h1>This is title functional component!</h1>;
+};
+
+const HeaderComponent = () => {
+    return(
+        <div className="Title" key="title">
+            {element}
+            <h1 style={{color: "orange"}}>This is h1 tag</h1>
+            <TitleFnComponent/>
+            <h2 style={{color: "blue"}}>This is h2 tag</h2>
+            <TitleFnComponent></TitleFnComponent>
+            <h3 style={{color: "green"}}>This is h3 tag</h3>
+        </div>
+    );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // rendering the component
-root.render(<HeadingComponent />);
+root.render(<HeaderComponent/>);
